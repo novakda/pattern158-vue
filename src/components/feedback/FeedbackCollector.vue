5289da4acf5b4d188bedc3bb651251550f68a2d7
xhiris <xhiris@gmail.com>
2026-04-03 16:12:12 -0700

    feat(25-02): create FeedbackTrigger FAB and FeedbackCollector orchestrator
    - FeedbackTrigger: 48x48 bug icon FAB with disabled state and a11y
    - FeedbackCollector: Teleport-based root with composable wiring
    - No style blocks -- all CSS from feedback.css side-effect import

---
+++ src/components/feedback/FeedbackCollector.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import './feedback.css'
+import FeedbackTrigger from './FeedbackTrigger.vue'
+import { useFeedbackConfig } from '@/composables/useFeedbackConfig'
+import { useFeedback } from '@/composables/useFeedback'
+
+const config = useFeedbackConfig()
+const feedback = useFeedback()
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="fb-root">
+      <FeedbackTrigger
+        :disabled="!config.isConfigured"
+        @activate="feedback.activate()"
+      />
+    </div>
+  </Teleport>
+</template>
